d6b82d2f4e2cbc2e6aacb5602deea7d223af3218
JayD2003 <jayasaidodla@gmail.com>
2024-04-13 00:41:10 +0530

    added extra files and components

---
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+export const Navbar = () => {
+    return (
+        <nav className={styles.container}>
+            <h1>My Portfolio</h1>
+        </nav>
+    );
+}
